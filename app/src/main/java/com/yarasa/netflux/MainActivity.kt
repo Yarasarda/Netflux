d0c88d87826f9d8efd622bdfa35ebc89bc0918f1
Arda Kaan <108005921+Yarasarda@users.noreply.github.com>
2025-12-11 23:55:10 +0300

    first build

---
+++ app/src/main/java/com/yarasa/netflux/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.yarasa.netflux
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yarasa.netflux.ui.theme.NetfluxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NetfluxTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)){
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NetfluxTheme {
+
+    }
+}
